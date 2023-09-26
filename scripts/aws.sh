c44328b4662a3e99eebe46627aadcf259a88d78d
Miguel Gargallo <it@itamaesan.org>
2023-09-26 13:19:10 +0200

    Update aws.sh

--- scripts/aws.sh
+++ scripts/aws.sh
@@ -1,4 +1,6 @@
 # Install AWS CLI
 RUN curl "https://d1vvhvl2y92vvt.cloudfront.net/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && \
     unzip awscliv2.zip && \
-    sudo ./aws/install
+    sudo ./aws/install && \
+    rm -rf awscliv2.zip && \
+    aws --version
