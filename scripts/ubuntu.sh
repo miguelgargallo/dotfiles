18c99ab1a6c20d82b84536cc63911714982279ad
Miguel Gargallo <it@itamaesan.org>
2023-09-26 14:01:06 +0200

    Update ubuntu.sh

--- scripts/ubuntu.sh
+++ scripts/ubuntu.sh
@@ -1,1 +1,1 @@
-apt-get update && apt-get upgrade -y && apt-get install -y sudo nano vim neovim ufw zip unzip certbot wget curl git gh neofetch build-essential gcc make fontconfig zsh fonts-font-awesome
+apt-get update && apt-get upgrade -y && apt-get install -y sudo nano vim neovim ufw zip unzip certbot wget curl git gh neofetch build-essential gcc make fontconfig zsh fonts-font-awesome apt-transport-https ca-certificates software-properties-common
