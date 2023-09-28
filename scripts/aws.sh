24c988adec47d503b819795974c2592de51e40f3
Miguel Gargallo <mgargallo16@gmail.com>
2023-09-28 18:05:35 +0200

    gh nosudos

--- scripts/aws.sh
+++ scripts/aws.sh
@@ -1,1 +1,1 @@
-curl "https://d1vvhvl2y92vvt.cloudfront.net/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && unzip awscliv2.zip && sudo ./aws/install && rm -rf awscliv2.zip && aws --version
+sudo curl "https://d1vvhvl2y92vvt.cloudfront.net/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && sudo unzip awscliv2.zip && sudo ./aws/install && sudo rm -rf awscliv2.zip && aws --version
---
+++ scripts/awsNoSudo.sh
@@ -0,0 +1,1 @@
+curl "https://d1vvhvl2y92vvt.cloudfront.net/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && unzip awscliv2.zip && ./aws/install && rm -rf awscliv2.zip && aws --version
